9bbac7806bd0eb8d7dc60941016016c9cc9b7782
Elizabeth O'Neill <elizabethoneill5678@gmail.com>
2018-01-18 16:48:47 -0500

    Commit to merge with master so it can actually compile

--- src/org/usfirst/frc/team342/robot/RobotMap.java
+++ src/org/usfirst/frc/team342/robot/RobotMap.java
@@ -14,6 +14,11 @@ package org.usfirst.frc.team342.robot;
  * floating around.
  */
 public class RobotMap {
+	public static final int FRONT_LEFT_WHEEL_CAN_TALON = 1;
+	public static final int FRONT_RIGHT_WHEEL_CAN_TALON = 2;
+	public static final int BACK_LEFT_WHEEL_CAN_TALON = 3;
+	public static final int BACK_RIGHT_WHEEL_CAN_TALON = 4;
+	public static final int CENTER_WHEEL_CAN_TALON = 5;
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
 	// public static int leftMotor = 1;
