e3e5ef73ef4683b7e7265800e643935637adbe56
Nicholas Mims <nicholaslmims@gmail.com>
2018-01-27 11:01:57 -0500

    Commit to start working on subsystems

--- src/org/usfirst/frc/team342/robot/subsystems/CubeController.java
+++ src/org/usfirst/frc/team342/robot/subsystems/CubeController.java
@@ -1,12 +1,29 @@
 package org.usfirst.frc.team342.robot.subsystems;
 
+import org.usfirst.frc.team342.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class CubeController extends Subsystem {
 
 	private static final CubeController INSTANCE = new CubeController();
 
+	private Solenoid pneumaticClaw;
+	private TalonSRX intakeMaster;
+	private TalonSRX intakeFollow;
+	private AnalogInput infraredSensor;
+
+	private static final boolean OPEN = true;
+	private static final boolean CLOSED = false;
+
 	public CubeController() {
+
 		initializeCubeController();
 	}
 
@@ -17,43 +34,65 @@ public class CubeController extends Subsystem {
 	}
 
 	public static CubeController getInstance() {
+
 		return INSTANCE;
 	}
 
 	private void initializeCubeController() {
-		// TODO Add Code
+
+		pneumaticClaw = new Solenoid(RobotMap.PNEUMATICCLAW);
+		intakeMaster = new WPI_TalonSRX(RobotMap.INTAKEMASTER);
+		intakeFollow = new WPI_TalonSRX(RobotMap.INTAKEFOLLOW);
+		infraredSensor = new AnalogInput(RobotMap.INFRAREDSENSOR);
+
+		intakeFollow.follow(intakeMaster);
 	}
 
 	public void collectCube(double speed) {
-		// TODO Add Code
+
+		pneumaticClaw.set(OPEN);
+		intakeMaster.set(ControlMode.PercentOutput, speed);
+
 	}
 
 	public void dispenseCube(double speed) {
-		// TODO Add Code
+
+		pneumaticClaw.set(OPEN);
+		intakeMaster.set(ControlMode.PercentOutput, speed);
 	}
 
 	public void collectorOutForce(double speed) {
-		// TODO Add Code
+		
+		pneumaticClaw.set(OPEN);
+		intakeMaster.set(ControlMode.PercentOutput, speed);
 	}
 
 	public void collectorInForce(double speed) {
-		// TODO Add Code
+
+		pneumaticClaw.set(CLOSED);
+		intakeMaster.set(ControlMode.PercentOutput, speed);
 	}
-	
+
 	public boolean getPneumaticClawState() {
-		return false;
+
+		return pneumaticClaw.get();
 	}
 
 	public double getInfrared() {
-		return 0.0;
+
+		// \/ Is this the right method to use to get an analog output? \/
+		return infraredSensor.getAverageVoltage();
 	}
 
 	public void collectorStop() {
-		// TODO Add Code
+		intakeMaster.set(ControlMode.PercentOutput, 0.0);
+		pneumaticClaw.set(CLOSED);
 	}
 
 	public void stopAll() {
-		// TODO Add Code
+		intakeMaster.set(ControlMode.PercentOutput, 0.0);
+		intakeMaster.set(ControlMode.PercentOutput, 0.0);
+		pneumaticClaw.set(getPneumaticClawState());
 	}
 
 }
