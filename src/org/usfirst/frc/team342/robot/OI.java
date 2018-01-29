70ad26d4811def2628b5239be6db60882aa4899b
Elizabeth O'Neill <elizabethoneill5678@gmail.com>
2018-01-29 18:13:46 -0500

    I finished oi.java

--- src/org/usfirst/frc/team342/robot/OI.java
+++ src/org/usfirst/frc/team342/robot/OI.java
@@ -18,60 +18,101 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class OI {
 	private static final OI instance = new OI ();
+	
 	 // Create the joystick and the 6 buttons on it
-		Joystick leftJoy = new Joystick(1);
-		 Button leftJoy_trigger = new JoystickButton(leftJoy, 1),
-				leftJoy_button2 = new JoystickButton(leftJoy, 2),
-				leftJoy_button3 = new JoystickButton(leftJoy, 3),
-				leftJoy_button4 = new JoystickButton(leftJoy, 4),
-				leftJoy_button5 = new JoystickButton(leftJoy, 5),
-				leftJoy_button6 = new JoystickButton(leftJoy, 6),
-				leftJoy_button7 = new JoystickButton(leftJoy, 7),
-				leftJoy_button8 = new JoystickButton(leftJoy, 8);
-		Joystick rightJoy = new Joystick(2);
-		 Button rightJoy_trigger = new JoystickButton(rightJoy, 1),
-				rightJoy_button2 = new JoystickButton(rightJoy, 2),
-				rightJoy_button3 = new JoystickButton(rightJoy, 3),
-				rightJoy_button4 = new JoystickButton(rightJoy, 4),
-				rightJoy_button5 = new JoystickButton(rightJoy, 5),
-				rightJoy_button6 = new JoystickButton(rightJoy, 6),
-				rightJoy_button7 = new JoystickButton(rightJoy, 7),
-				rightJoy_button8 = new JoystickButton(rightJoy, 8);
-		Joystick rightPad = new Joystick(3);
-		Button  rightPad_trigger = new JoystickButton(rightPad, 1),
-				rightPad_button2 = new JoystickButton(rightPad, 2),
-				rightPad_button3 = new JoystickButton(rightPad, 3),
-				rightPad_button4 = new JoystickButton(rightPad, 4),
-				rightPad_button5 = new JoystickButton(rightPad, 5),
-				rightPad_button6 = new JoystickButton(rightPad, 6),
-				rightPad_button7 = new JoystickButton(rightPad, 7),
-				rightPad_button8 = new JoystickButton(rightPad, 8);
-		Joystick leftPad = new Joystick(4);
-		Button leftPad_trigger = new JoystickButton(leftPad, 1),
-				leftPad_button2 = new JoystickButton(leftPad, 2),
-				leftPad_button3 = new JoystickButton(leftPad, 3),
-				leftPad_button4 = new JoystickButton(leftPad, 4),
-				leftPad_button5 = new JoystickButton(leftPad, 5),
-				leftPad_button6 = new JoystickButton(leftPad, 6),
-				leftPad_button7 = new JoystickButton(leftPad, 7),
-				leftPad_button8 = new JoystickButton(leftPad, 8);
+		Joystick leftJoy;
+		 Button leftJoy_trigger,
+				leftJoy_button2 ,
+				leftJoy_button3 ,
+				leftJoy_button4 ,
+				leftJoy_button5 ,
+				leftJoy_button6 ,
+				leftJoy_button7,
+				leftJoy_button8 ;
+		Joystick rightJoy ;
+		 Button rightJoy_trigger ,
+				rightJoy_button2 ,
+				rightJoy_button3 ,
+				rightJoy_button4 ,
+				rightJoy_button5 ,
+				rightJoy_button6 ,
+				rightJoy_button7 ,
+				rightJoy_button8 ;
+		Joystick rightPad;
+		Button  rightPad_trigger ,
+				rightPad_button2 ,
+				rightPad_button3 ,
+				rightPad_button4 ,
+				rightPad_button5 ,
+				rightPad_button6 ,
+				rightPad_button7 ,
+				rightPad_button8 ;
+		Joystick leftPad;
+		Button leftPad_trigger ,
+				leftPad_button2 ,
+				leftPad_button3 ,
+				leftPad_button4 ,
+				leftPad_button5 ,
+				leftPad_button6 ,
+				leftPad_button7 ,
+				leftPad_button8 ;
 
 
 	private OI() {
-		
-		
-		//drive
-		
-
-		//cube controller
-		
-		//climb
-		
-		//pneumatics
-		
-		//lift system
+		 leftJoy = new Joystick(1);
+		  leftJoy_trigger = new JoystickButton(leftJoy, 1);
+				leftJoy_button2 = new JoystickButton(leftJoy, 2);
+				leftJoy_button3 = new JoystickButton(leftJoy, 3);
+				leftJoy_button4 = new JoystickButton(leftJoy, 4);
+				leftJoy_button5 = new JoystickButton(leftJoy, 5);
+				leftJoy_button6 = new JoystickButton(leftJoy, 6);
+				leftJoy_button7 = new JoystickButton(leftJoy, 7);
+				leftJoy_button8 = new JoystickButton(leftJoy, 8);
+		  rightJoy = new Joystick(2);
+		  rightJoy_trigger = new JoystickButton(rightJoy, 1);
+				rightJoy_button2 = new JoystickButton(rightJoy, 2);
+				rightJoy_button3 = new JoystickButton(rightJoy, 3);
+				rightJoy_button4 = new JoystickButton(rightJoy, 4);
+				rightJoy_button5 = new JoystickButton(rightJoy, 5);
+				rightJoy_button6 = new JoystickButton(rightJoy, 6);
+				rightJoy_button7 = new JoystickButton(rightJoy, 7);
+				rightJoy_button8 = new JoystickButton(rightJoy, 8);
+		 rightPad = new Joystick(3);
+		  rightPad_trigger = new JoystickButton(rightPad, 1);
+				rightPad_button2 = new JoystickButton(rightPad, 2);
+				rightPad_button3 = new JoystickButton(rightPad, 3);
+				rightPad_button4 = new JoystickButton(rightPad, 4);
+				rightPad_button5 = new JoystickButton(rightPad, 5);
+				rightPad_button6 = new JoystickButton(rightPad, 6);
+				rightPad_button7 = new JoystickButton(rightPad, 7);
+				rightPad_button8 = new JoystickButton(rightPad, 8);
+		 leftPad = new Joystick(4);
+		leftPad_trigger = new JoystickButton(leftPad, 1);
+				leftPad_button2 = new JoystickButton(leftPad, 2);
+				leftPad_button3 = new JoystickButton(leftPad, 3);
+				leftPad_button4 = new JoystickButton(leftPad, 4);
+				leftPad_button5 = new JoystickButton(leftPad, 5);
+				leftPad_button6 = new JoystickButton(leftPad, 6);
+				leftPad_button7 = new JoystickButton(leftPad, 7);
+				leftPad_button8 = new JoystickButton(leftPad, 8);
 }
 	public static OI getInstance () {
 		return instance;
+	}
+	
+	public Joystick getJoystickLeftDrive () {
+		return leftJoy ;
+	}
+	
+	public Joystick getJoystickRightDrive () {
+		return rightJoy ;
+	}
+	
+	public Joystick getJoypadLeftDrive () {
+		return leftPad ; 
+	}
+	
+	public Joystick getJoypadRightDrive () {
+		return rightPad ;
 	}
 }
