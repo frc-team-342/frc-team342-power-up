36c23735cd3e5ff6ff46251666262a61145a9d02
Nicholas Mims <nicholaslmims@gmail.com>
2018-01-29 16:35:58 -0500

    Commit to add all the subsystems and to set up the robotmap part 2

--- src/org/usfirst/frc/team342/robot/subsystems/LightsSubsystem.java
+++ src/org/usfirst/frc/team342/robot/subsystems/LightsSubsystem.java
@@ -15,12 +15,12 @@ public class LightsSubsystem extends Subsystem {
 	
 	private DriverStation ds;
 	
-	private PWM red;
-	private PWM green;
-	private PWM blue;
-	private PWM twelveVoltsOne;
-	private PWM twelveVoltsTwo;
-	private PWM fiveVolts;
+	//private PWM red;
+	//private PWM green;
+	//private PWM blue;
+	//private PWM twelveVoltsOne;
+	//private PWM twelveVoltsTwo;
+	//private PWM fiveVolts;
 	
 	public LightsSubsystem() {
 		initializeLightsSubsystem();
@@ -38,49 +38,49 @@ public class LightsSubsystem extends Subsystem {
 	}
 	
 	private void initializeLightsSubsystem() {
-		red = new PWM(RobotMap.RED);
-		green = new PWM(RobotMap.GREEN);
-		blue = new PWM(RobotMap.BLUE);
-		twelveVoltsOne = new PWM(RobotMap.TWELVEVOLTSONE);
-		twelveVoltsTwo = new PWM(RobotMap.TWELVEVOLTSTWO);
-		fiveVolts = new PWM(RobotMap.FIVEVOLTS);
+		//red = new PWM(RobotMap.RED);
+		//green = new PWM(RobotMap.GREEN);
+		//blue = new PWM(RobotMap.BLUE);
+		//twelveVoltsOne = new PWM(RobotMap.TWELVEVOLTSONE);
+		//twelveVoltsTwo = new PWM(RobotMap.TWELVEVOLTSTWO);
+		//fiveVolts = new PWM(RobotMap.FIVEVOLTS);
 		
-		ds = DriverStation.getInstance();
+		//ds = DriverStation.getInstance();
 	}
 	
 	public void setDriverStationColor() {
-		if(ds.getAlliance().equals(Alliance.Red)){
-			setRed(255);
-			setGreen(0);
-			setBlue(0);
-		}else if(ds.getAlliance().equals(Alliance.Blue)) {
-			setBlue(255);
-			setGreen(0);
-			setRed(0);
-		}
+		//if(ds.getAlliance().equals(Alliance.Red)){
+		//	setRed(255);
+		//	setGreen(0);
+		//	setBlue(0);
+		//}else if(ds.getAlliance().equals(Alliance.Blue)) {
+		//	setBlue(255);
+		//	setGreen(0);
+		//	setRed(0);
+		//}
 	}
 	
 	public void setRed(int value) {
-		red.setRaw(value);
+		//red.setRaw(value);
 	}
 	
 	public void setGreen(int value) {
-		green.setRaw(value);
+		//green.setRaw(value);
 	}
 	
 	public void setBlue(int value) {
-		blue.setRaw(value);
+		//blue.setRaw(value);
 	}
 	
 	public void setTwelveVoltsOne(int value) {
-		twelveVoltsOne.setRaw(value);
+		//twelveVoltsOne.setRaw(value);
 	}
 	
 	public void setTwelveVoltsTwo(int value) {
-		twelveVoltsTwo.setRaw(value);
+		//twelveVoltsTwo.setRaw(value);
 	}
 	
 	public void setFiveVolts(int value) {
-		fiveVolts.setRaw(value);
+		//fiveVolts.setRaw(value);
 	}
 }
